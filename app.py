54f2e8f31bcdd0fb251c1df2d0e74ad86a68fe02
Elias <Elias.johansson.elle.93@gmail.com>
2019-07-18 18:03:03 +0200

    app.py updated to use postgres

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
+import os
+
 from flask import Flask, render_template
+import psycopg2
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    record = 'No records found...'
+    connection = None
+    try:
+        DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://folkol@localhost:5432/planning-tool')
+        connection = psycopg2.connect(DATABASE_URL)
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print(connection.get_dsn_parameters(), "\n")
+        # Print PostgreSQL version
+        cursor.execute("SELECT * FROM activities")
+        record = cursor.fetchone()
+        print("You are connected to - ", record, "\n")
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        # closing database connection.
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+    return str(record or 'No records found...')
 
 
 if __name__ == '__main__':
