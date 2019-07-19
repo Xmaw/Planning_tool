befd9bdb039988f34ef7425fe3e1dd5f12d78a9c
Elias <Elias.johansson.elle.93@gmail.com>
2019-07-19 15:18:42 +0200

    add_activity() updated to add informaiton to the remote database.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
         print(connection.get_dsn_parameters(), "\n")
         # Print PostgreSQL version
         cursor.execute("SELECT * FROM activities")
-        record = cursor.fetchone()
+        record = cursor.fetchall()
         print("You are connected to - ", record, "\n")
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
@@ -28,10 +28,41 @@ def index():
             cursor.close()
             connection.close()
             print("PostgreSQL connection is closed")
-    return str(record or 'No records found...')
+    return render_template('index.html')
+
+
+def add_activity(title, start, stop):
+    record = 'No records found...'
+    connection = None
+    try:
+        DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://folkol@localhost:5432/planning-tool')
+        connection = psycopg2.connect(DATABASE_URL)
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print(connection.get_dsn_parameters(), "\n")
+        # Print PostgreSQL version
+
+        cursor.execute(f'''INSERT INTO public.activities (
+              title
+            , start
+            , stop
+        ) VALUES (
+              %s -- title character varying
+            , %s -- start time without time zone
+            , %s -- stop time without time zone
+        )''', (title, start, stop))
+
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        # closing database connection.
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
-
-
